4ff06c54147e6258160ba3dd98d0484f3a748346
flyinglove <18710550789@sina.cn>
2016-06-24 09:14:49 +0800

    使程序可以适用于多个文件
    之前程序打开的文件是在内部指定的mbox.txt，不可以改动，但现在可以指定文件，此外，输出行的文件名之前包含路径，但现在已经从完整路径中摘出了文件名

--- chapter11/grep.py
+++ chapter11/grep.py
@@ -2,13 +2,15 @@
 import re;
 
 reg_str = raw_input("Enter a regular expression:");
-
-fhand = open('../mbox.txt');
-
+fname = raw_input("Enter a file name:");
+fhand = open(fname);
+#fhand = open('../mbox.txt');
+if fname.find('/') != -1:
+	fname = re.findall('\/([^<>/\*?|:]+\.*$)', fname)[0]
 count = 0;
 
 for lines in fhand:
 	if re.search(reg_str, lines):
 		count = count + 1;
 # 格式化序列
-print "mbox-txt had %d lines that matched %s ." % (count, reg_str);
+print "%s had %d lines that matched %s ." % (fname, count, reg_str);
